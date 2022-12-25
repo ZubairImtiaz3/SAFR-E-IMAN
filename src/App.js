0ea16d52b38f52d528b725ac0b26e55722554fe3
Zubair <zubairimtiaz395@gmail.com>
2022-12-26 00:29:25 +0500

    Favicon Added + Terminal Cleared

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PropagateLoader from "react-spinners/PropagateLoader";
 import "./App.css";
 
