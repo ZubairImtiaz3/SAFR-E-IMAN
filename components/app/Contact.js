b85a662e597d91fb99d8eb42dfaf49ad6926fa58
MoazMirza-13 <moazmirza13@gmail.com>
2023-07-23 15:08:02 +0500

    improved ux

--- components/app/Contact.js
+++ components/app/Contact.js
@@ -10,6 +10,7 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import Modal from "../../components/ui/Modal";
 import Iframe from "./map/Iframe";
+import Animatedpage from "@/components/app/route/Animatedpage";
 
 //Yup Validation
 const schema = yup.object().shape({
@@ -75,166 +76,173 @@ export default function Contact() {
         target={0}
         breakpoint={2560}
       />
-      <div className="illustrationContact max-w-[35%] mx-auto mt-20">
-        <Image src={conatctImg} alt="img" />
-      </div>
-
-      <section
-        className={`contact w-full max-w-3xl px-6 py-4 mx-auto bg-white rounded-md shadow-md mt-20`}
-      >
-        <h2 className="text-3xl font-Raleway font-semibold text-center text-gray-800">
-          Get in touch
-        </h2>
-
-        <p className="mt-3 font-Poppins text-center text-gray-600">
-          Discover hidden gems, book accommodations, and create unforgettable
-          memories.
-        </p>
-
-        <div className="font-Poppins grid grid-cols-1 gap-6 mt-6 sm:grid-cols-2 md:grid-cols-3">
-          <a
-            href="https://goo.gl/maps/tTEhxaUZiVWLvC9N7"
-            className="flex flex-col items-center px-4 py-3 text-gray-700 transition-colors duration-300 transform rounded-md  hover:bg-[#efbe699f]"
-          >
-            <svg
-              className="w-5 h-5"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
-                clipRule="evenodd"
-              />
-            </svg>
-
-            <span className="mt-2">Qila Ahmad Abad</span>
-          </a>
-
-          <a
-            href="https://wa.me/923446463437"
-            className="flex flex-col items-center px-4 py-3 text-gray-700 transition-colors duration-300 transform rounded-md  hover:bg-[#efbe699f]"
-          >
-            <svg
-              className="w-5 h-5"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
-            </svg>
-
-            <span className="mt-2">+923472709395</span>
-          </a>
-
-          <a
-            rel="noreferrer"
-            href="https://mail.google.com/mail/?view=cm&fs=1&to=safinaalmadina@gmail.com"
-            target="_blank"
-            className="flex flex-col items-center px-4 py-3 text-gray-700 transition-colors duration-300 transform rounded-md  hover:bg-[#efbe699f]"
-          >
-            <svg
-              className="w-5 h-5"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-              <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-            </svg>
-
-            <span className="mt-2">Safinaalmadina</span>
-          </a>
+      <Animatedpage>
+        <div className="illustrationContact max-w-[35%] mx-auto mt-20">
+          <Image src={conatctImg} alt="img" />
         </div>
 
-        <div className="mt-6 ">
-          <form ref={form} onSubmit={handleSubmit(sendEmail)}>
-            <div className="font-poppins items-center -mx-2 md:flex">
-              <div className="w-full mx-2">
-                <label className="block mb-2 text-sm font-medium text-gray-600 ">
-                  Name
-                </label>
+        <section
+          className={`contact w-full max-w-3xl px-6 py-4 mx-auto bg-white rounded-md shadow-md mt-20`}
+        >
+          <h2 className="text-3xl font-Raleway font-semibold text-center text-gray-800">
+            Get in touch
+          </h2>
 
-                <input
-                  {...register("from_name")}
-                  className={`block w-full px-4 py-2 text-gray-700 bg-white border rounded-md 
-                  ${errors.from_name ? "border-red-500" : ""}`}
-                  type="text"
-                />
-                {errors.from_name && (
-                  <p className="text-red-400 mt-1">
-                    {errors.from_name.message}
-                  </p>
-                )}
-              </div>
+          <p className="mt-3 font-Poppins text-center text-gray-600">
+            Discover hidden gems, book accommodations, and create unforgettable
+            memories.
+          </p>
 
-              <div className="w-full mx-2 mt-4 md:mt-0">
-                <label className="block mb-2 text-sm font-medium text-gray-600 ">
-                  E-mail
-                </label>
-
-                <input
-                  {...register("from_email")}
-                  className={`block w-full px-4 py-2 text-gray-700 bg-white border rounded-md 
-                  ${errors.from_email ? "border-red-500" : ""}`}
-                  type="email"
-                />
-                {errors.from_email && (
-                  <p className="text-red-400 mt-1">
-                    {errors.from_email.message}
-                  </p>
-                )}
-              </div>
-            </div>
-
-            <div className="w-full mt-4">
-              <label className="block mb-2 text-sm font-medium text-gray-600 ">
-                Message
-              </label>
-
-              <textarea
-                {...register("message")}
-                className={`block w-full h-40 px-4 py-2 text-gray-700 bg-white border rounded-md 
-                ${errors.message ? "border-red-500" : ""}`}
-              ></textarea>
-              {errors.message && (
-                <p className="text-red-400 mt-1">{errors.message.message}</p>
-              )}
-            </div>
-
-            <div className="font-Poppins flex justify-center mt-6">
-              <button
-                type="submit"
-                value="Send"
-                className="px-4 py-2 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
+          <div className="font-Poppins grid grid-cols-1 gap-6 mt-6 sm:grid-cols-2 md:grid-cols-3">
+            <a
+              href="https://goo.gl/maps/tTEhxaUZiVWLvC9N7"
+              className="flex flex-col items-center px-4 py-3 text-gray-700 transition-colors duration-300 transform rounded-md  hover:bg-[#efbe699f]"
+            >
+              <svg
+                className="w-5 h-5"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
               >
-                {loading ? (
-                  <span className="flex justify-center items-center gap-2">
-                    Sending... <ClipLoader size={17} color="white" />
-                  </span>
-                ) : (
-                  <span>Send Message</span>
-                )}
-              </button>
-            </div>
-          </form>
-        </div>
-      </section>
+                <path
+                  fillRule="evenodd"
+                  d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+                  clipRule="evenodd"
+                />
+              </svg>
 
-      {modal && (
-        <Modal onClose={closePopup} activeModal={true} title="Success" centered>
-          <div className="">
-            <p className="text-white leading-5">
-              Your Queries Submitted Successfully. Our Team will reach out to
-              you soon.
-              <br />
-              THANKYOU !
-            </p>
+              <span className="mt-2">Qila Ahmad Abad</span>
+            </a>
+
+            <a
+              href="https://wa.me/923446463437"
+              className="flex flex-col items-center px-4 py-3 text-gray-700 transition-colors duration-300 transform rounded-md  hover:bg-[#efbe699f]"
+            >
+              <svg
+                className="w-5 h-5"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+              </svg>
+
+              <span className="mt-2">+923472709395</span>
+            </a>
+
+            <a
+              rel="noreferrer"
+              href="https://mail.google.com/mail/?view=cm&fs=1&to=safinaalmadina@gmail.com"
+              target="_blank"
+              className="flex flex-col items-center px-4 py-3 text-gray-700 transition-colors duration-300 transform rounded-md  hover:bg-[#efbe699f]"
+            >
+              <svg
+                className="w-5 h-5"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+                <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+              </svg>
+
+              <span className="mt-2">Safinaalmadina</span>
+            </a>
           </div>
-        </Modal>
-      )}
-      <Iframe />
+
+          <div className="mt-6 ">
+            <form ref={form} onSubmit={handleSubmit(sendEmail)}>
+              <div className="font-poppins items-center -mx-2 md:flex">
+                <div className="w-full mx-2">
+                  <label className="block mb-2 text-sm font-medium text-gray-600 ">
+                    Name
+                  </label>
+
+                  <input
+                    {...register("from_name")}
+                    className={`block w-full px-4 py-2 text-gray-700 bg-white border rounded-md 
+                  ${errors.from_name ? "border-red-500" : ""}`}
+                    type="text"
+                  />
+                  {errors.from_name && (
+                    <p className="text-red-400 mt-1">
+                      {errors.from_name.message}
+                    </p>
+                  )}
+                </div>
+
+                <div className="w-full mx-2 mt-4 md:mt-0">
+                  <label className="block mb-2 text-sm font-medium text-gray-600 ">
+                    E-mail
+                  </label>
+
+                  <input
+                    {...register("from_email")}
+                    className={`block w-full px-4 py-2 text-gray-700 bg-white border rounded-md 
+                  ${errors.from_email ? "border-red-500" : ""}`}
+                    type="email"
+                  />
+                  {errors.from_email && (
+                    <p className="text-red-400 mt-1">
+                      {errors.from_email.message}
+                    </p>
+                  )}
+                </div>
+              </div>
+
+              <div className="w-full mt-4">
+                <label className="block mb-2 text-sm font-medium text-gray-600 ">
+                  Message
+                </label>
+
+                <textarea
+                  {...register("message")}
+                  className={`block w-full h-40 px-4 py-2 text-gray-700 bg-white border rounded-md 
+                ${errors.message ? "border-red-500" : ""}`}
+                ></textarea>
+                {errors.message && (
+                  <p className="text-red-400 mt-1">{errors.message.message}</p>
+                )}
+              </div>
+
+              <div className="font-Poppins flex justify-center mt-6">
+                <button
+                  type="submit"
+                  value="Send"
+                  className="px-4 py-2 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
+                >
+                  {loading ? (
+                    <span className="flex justify-center items-center gap-2">
+                      Sending... <ClipLoader size={17} color="white" />
+                    </span>
+                  ) : (
+                    <span>Send Message</span>
+                  )}
+                </button>
+              </div>
+            </form>
+          </div>
+        </section>
+
+        {modal && (
+          <Modal
+            onClose={closePopup}
+            activeModal={true}
+            title="Success"
+            centered
+          >
+            <div className="">
+              <p className="text-white leading-5">
+                Your Queries Submitted Successfully. Our Team will reach out to
+                you soon.
+                <br />
+                THANKYOU !
+              </p>
+            </div>
+          </Modal>
+        )}
+        <Iframe />
+      </Animatedpage>
     </>
   );
 }
