7ad8a173f98ec916d134e2c121d655b9177aaef6
Zubair Imtiaz <zubairimtiaz395@gmail.com>
2023-07-24 06:02:06 +0500

    PreLoader Added

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,13 @@
+"use client";
+import { useState, useEffect } from "react";
+import dynamic from "next/dynamic";
 import Navbar from "@/components/app/Navbar";
 import Infobar from "@/components/app/Infobar";
 import Footer from "@/components/app/Footer";
 import "./globals.css";
 import { Raleway, Poppins } from "next/font/google";
+import logoLoader from "@/public/assets/partials/logo2.svg";
+import Image from "next/image";
 
 const raleway = Raleway({
   subsets: ["latin"],
@@ -17,6 +22,12 @@ const poppins = Poppins({
   weight: "400",
   variable: "--font-poppins",
 });
+
+// Dynamic import of PropagateLoader to avoid server-side rendering
+const PropagateLoader = dynamic(
+  () => import("react-spinners/PropagateLoader"),
+  { ssr: false }
+);
 
 export const metadata = {
   title: {
@@ -34,14 +45,37 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2500);
+  }, []);
+
   return (
     <>
       <html>
         <body lang="en" className={`${raleway.variable} ${poppins.variable}`}>
-          <Infobar />
-          <Navbar />
-          {children}
-          <Footer />
+          {loading ? (
+            <div className="loader">
+              <div className="loaderContent relative -top-16 sm:static">
+                <Image
+                  className="w-[290px] sm:w-auto sm:relative sm:left-[-7%]"
+                  src={logoLoader}
+                  alt="loader"
+                />
+                <PropagateLoader color="#ceb041" />
+              </div>
+            </div>
+          ) : (
+            <>
+              <Infobar />
+              <Navbar />
+              {children}
+              <Footer />
+            </>
+          )}
         </body>
       </html>
     </>
